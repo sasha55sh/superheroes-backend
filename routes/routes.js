a937c4d818f0fbfc9a2a3732a73356070c32f5c1
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-24 17:01:01 +0300

    features: creates routes for get, delete, post, put, patch data

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const SuperheroController = require("../controllers/SuperheroController");
+const ImagesControllers = require("../controllers/ImagesController");
+
 router.get("/", (req, res) => {
   res.send("Choose route");
 });
+
+router.get("/catalog", SuperheroController.getAllSuperheroes);
+router.get("/catalog/:id", SuperheroController.getSuperheroById);
+
+router.delete("/catalog/:id", SuperheroController.deleteSuperheroById);
+
+router.post("/catalog/create", SuperheroController.createSuperhero);
+
+router.put("/catalog/:id", ImagesControllers.updateImages);
+router.patch("/catalog/:id", SuperheroController.updateSuperheroData);
 
 module.exports = router;
