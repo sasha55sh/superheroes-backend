6c024004f048d45980e977bcc84405639b974005
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-24 16:57:05 +0300

    features: create config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 module.exports = {
   server: {
@@ -11,4 +18,6 @@ module.exports = {
   database: {
     uri: process.env.DB_URI,
   },
+
+  cloudinary
 };
